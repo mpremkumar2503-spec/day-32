6f49e4bf740666c187cee3eae08459d20a371be0
mpremkumar2503-spec <m.premkumar2503@gmail.com>
2026-02-14 08:43:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,57 @@
+import express from "express"
+
+const app = express();
+const PORT = 5000
+ 
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
+//what is the different between node and express?
+//Node.js	                              -                Express.js
+//Runtime environment	                  -                 Framework
+//Runs JavaScript outside                 -     browser	Built on top of Node
+//Provides core modules (http, fs, path)  -   	Makes server building easier
+//More low-level	                      -      High-level & simple
+
+//Node = Engine
+// Express = Helper framework
+
+//what is .env?
+//.env file stores secret or configuration data.
+//Example:
+//PORT=5000
+//DB_PASSWORD=123456
+// We use package:
+
+//npm install dotenv
+
+
+//In server:
+
+require("dotenv").config();
+console.log(process.env.PORT);
+
+
+//Why?
+
+// 1 Hide passwords
+
+// 2 Change environment easily
+
+// 3 Secure configuration
+
+//what is listen()
+app.listen(5000)
+//It starts the server
+// It tells server to listen for requests on a port
+
+//Without listen() → Server will not run.
+
+
